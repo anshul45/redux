539a22564a8587a3fadff9f010cae7e370aee011
anshul45 <80778090+anshul45@users.noreply.github.com>
2022-12-14 06:43:44 +0530

    redux learn

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const amount = useSelector(state => state.amount);
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-light">
@@ -34,7 +36,7 @@ const Navbar = () => {
             </ul>
             <div>
               <button disabled={true} className="btn btn-primary">
-                Balance 10000
+                Balance {amount}
               </button>
             </div>
           </div>
